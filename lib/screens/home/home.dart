8c3e7edb5f87dba5294d41f3946bd10366390d72
Daigo <daigo3262@gmail.com>
2020-09-14 09:52:44 +0900

    settings icon on appbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -12,6 +12,16 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context){
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: Text('bottom sheet'),
+        );
+      });
+    }
+    
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -27,7 +37,11 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
-            )
+            ),
+            FlatButton.icon(
+                onPressed: () => _showSettingsPanel(),
+                icon: Icon(Icons.settings),
+                label: Text('settings'))
           ],
         ),
         body: BrewList(),
